a94e125550537d94fdf4564fc137613816bc46a8
Alex31TheDev <alexdraghia45@gmail.com>
2025-04-27 15:02:51 +0300

    Fix quotes

--- file_to_image.py
+++ file_to_image.py
@@ -2,22 +2,22 @@ from PIL import Image, ImageDraw
 import sys
 import math
 
-f = open(sys.argv[2], "rb")
+f = open(sys.argv[2], 'rb')
 f.seek(0, 2)
 
-if sys.argv[1] == "0":
+if sys.argv[1] == '0':
     w = math.ceil(math.sqrt(f.tell() / 3))
 else:
     w = math.ceil(math.sqrt(f.tell()))
 
-img = Image.new("RGBA", (w, w))
+img = Image.new('RGBA', (w, w))
 draw = ImageDraw.Draw(img)
 f.seek(0, 0)
 
 i = 0
 while True:
     b1 = f.read(1)
-    if sys.argv[1] == "0":
+    if sys.argv[1] == '0':
         b2 = f.read(1)
         b3 = f.read(1)
 
@@ -25,7 +25,7 @@ while True:
          break
 
     R = int.from_bytes(b1, 'little')
-    if sys.argv[1] == "0":
+    if sys.argv[1] == '0':
         G = int.from_bytes(b2, 'little')
         B = int.from_bytes(b3, 'little')
     else:
